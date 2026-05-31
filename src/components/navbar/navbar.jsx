e65c4aef1aab99d5e875b93f38e305ae1fb32d2c
Iancu Victor-Iuliu <iancu27victor@gmail.com>
2026-05-31 20:47:04 +0300

    added NavLinks instead of a tags. as opposed to links, navlinks have the isactive property which is really helpful with the styling.

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,14 +1,20 @@
+import { NavLink } from 'react-router';
+import BurnLogo from './assets/burnLogo.svg';
+
 function Navbar(){
-    return <div id='T'>
-        <a href="/">Acasa</a>
-        <a href="/abonamente">Abonamente</a>
-        <a href="/rezervari">Rezervări online</a>
-        <a href="/servicii">Servicii</a>
-        <a href="/orar-clase">Orar clase</a>
-        <a href="/blog">Blog</a>
-        <a href="/galerie">Galerie foto</a>
-        <a href="/salidefitness">Săli fitness</a>
-        <a href="/contact">Contact</a>
+    const buttonClass = ({isActive}) => isActive ? 'text-white underline underline-offset-5' : 'hover:text-white hover:underline underline-offset-5 duration-150'
+
+    return <div id='navbar' className='h-25 bg-black text-gray-500 text-[16px] flex gap-10 items-center justify-center'>
+        <img src={BurnLogo} alt="burn fitness logo" class='w-60'/>
+        <NavLink to="/" className={buttonClass}>Acasa</NavLink>
+        <NavLink to="/abonamente" className={buttonClass}>Abonamente</NavLink>
+        <NavLink to="/rezervari" className={buttonClass}>Rezervări online</NavLink>
+        <NavLink to="/servicii" className={buttonClass}>Servicii</NavLink>
+        <NavLink to="/orar-clase" className={buttonClass}>Orar clase</NavLink>
+        <NavLink to="/blog" className={buttonClass}>Blog</NavLink>
+        <NavLink to="/galerie" className={buttonClass}>Galerie foto</NavLink>
+        <NavLink to="/salidefitness" className={buttonClass}>Săli fitness</NavLink>
+        <NavLink to="/contact" className={buttonClass}>Contact</NavLink>
     </div>
 }
 
