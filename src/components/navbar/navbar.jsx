5d0334e4282baab07e7c7767e0ef61e07e61c94d
Iancu Victor-Iuliu <iancu27victor@gmail.com>
2026-05-31 22:58:58 +0300

    added social media icons from fontawesome library

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,6 +3,7 @@ import BurnLogo from './assets/burnLogo.svg';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookSquare, faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
 
 function Navbar(){
     const [menuState, setMenuState] = useState(false);
@@ -24,12 +25,12 @@ function Navbar(){
 
     {/* mobile navbar */}
     <div className='flex justify-between bg-black'>
-    <img src={BurnLogo} alt="burn fitness logo" className='w-40 m-[10px]'/>
+    <img src={BurnLogo} alt="burn fitness logo" className='md:hidden w-40 m-[10px]'/>
     <button 
     onClick={() => setMenuState(!menuState)}
     className={menuState ? 'hidden' : 'md:hidden cursor-pointer text-white text-[25px] pl-[px] pr-[20px]'}
     ><FontAwesomeIcon icon={faBars} /></button>
-    <div id='mobileBurger' className={menuState ? 'animate-fade-in flex flex-col fixed justify-center items-center gap-5 w-full h-full bg-black font-finlandica font-bold text-white text-[25px] md:hidden' : 'animate-fade-out hidden'}>
+    <div id='mobileBurger' className={menuState ? 'animate-fade-in flex flex-col fixed justify-center items-center gap-5 w-full h-full bg-black font-finlandica font-bold text-gray-500 text-[25px] md:hidden' : 'animate-fade-out hidden'}>
         <div onClick={() => setMenuState(!menuState)} className='fixed z-[-1] h-full w-full'></div>
         <img src={BurnLogo} alt="burn fitness logo" className='w-40 pb-[20px] box-border' onClick={() => setMenuState(!menuState)}/>
         <NavLink to="/" className={buttonClass} onClick={() => setMenuState(!menuState)}>Acasa</NavLink>
@@ -41,6 +42,10 @@ function Navbar(){
         <NavLink to="/galerie" className={buttonClass} onClick={() => setMenuState(!menuState)}>Galerie foto</NavLink>
         <NavLink to="/salidefitness" className={buttonClass} onClick={() => setMenuState(!menuState)}>Săli fitness</NavLink>
         <NavLink to="/contact" className={buttonClass} onClick={() => setMenuState(!menuState)}>Contact</NavLink>
+        <div>
+        <a href="https://www.instagram.com/burnfitnesscluj/" target='_blank'><FontAwesomeIcon icon={faInstagramSquare} className='text-[#E06397] hover:text-[#DB2777] duration-150 text-3xl'/></a>
+        <a href="https://www.facebook.com/BurnFitnessCluj" target='_blank'><FontAwesomeIcon icon={faFacebookSquare} className='text-[#6096D6] hover:text-[#1877F2] duration-150 text-3xl'/></a>
+        </div>
     </div>
     </div>
     </> 
