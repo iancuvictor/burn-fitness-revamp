c94d0297c2752451a6a4cb94e0f177f39b14143c
Iancu Victor-Iuliu <iancu27victor@gmail.com>
2026-06-01 13:43:06 +0300

    fixed navbar z index

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -8,7 +8,7 @@ function Navbar({menuState, setMenuState}){
     const buttonClass = ({isActive}) => isActive ? 'z-1 text-white underline underline-offset-5' : 'hover:text-white hover:underline underline-offset-5 duration-150'
 
     return <>
-    <div id='navbar' className='hidden md:flex h-20 gap-5 bg-black items-center md:justify-between xl:justify-evenly font-[600]'>
+    <div id='navbar' className='z-2 sticky top-0 w-full hidden md:flex h-20 gap-5 bg-black items-center md:justify-between xl:justify-evenly font-[600]'>
         <NavLink to="/" className={buttonClass}><img src={BurnLogo} alt="burn fitness logo" className='w-50 md:pl-[20px] select-none'/></NavLink>
         <div className='hidden md:flex flex-wrap text-gray-500 text-[16px] gap-2.5 xl:gap-5 items-center justify-end md:pr-[20px]'>
         <NavLink to="/" className={buttonClass}>Acasa</NavLink>
