1405b2b2586c6e17c342cd6a40e7432a6106d2bd
Iancu Victor-Iuliu <iancu27victor@gmail.com>
2026-06-08 09:21:04 +0300

    added post function for login

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import express from "express";
 import createServer from "express";
 import mongoose from "mongoose";
 import User from "./schemas/user.js";
+import bcrypt from 'bcrypt';
 
 const server = createServer();
 let port = process.env.PORT;
@@ -28,11 +29,35 @@ server.get("/api/users", (req, res) => {
 
 server.post('/api/users/register', async (req, res) => {
     console.log(req.body);
+    let hashedPass = await bcrypt.hash(req.body.password, 10)
     try {
-        await User.create({ username: req.body.username, password: req.body.password, email: req.body.email })
+        await User.create({ username: req.body.username, password: hashedPass, email: req.body.email });
+        console.log(`User with username: ${req.body.username} has registered now.`)
     } catch(err) {
         console.log(err)
         return res.json({ message: 'error creating user' })
     }
     res.json({ message: 'user created' })
+});
+
+server.post('/api/users/login', async (req, res) => {
+    console.log(req.body);
+    let userFound = await User.findOne({$or: [{username: req.body.username},{ email: req.body.email}]})
+    try {
+        if(userFound !== null){
+            if(await bcrypt.compare(req.body.password, userFound.password)){
+                console.log('Access permited');
+                res.json('Access permited');
+            } else {
+                console.log('Wrong password');
+                res.json('Wrong password');
+            }
+        } else if(userFound === null){
+            console.log('no user with this name or email is associated');
+            res.json('User not found');
+        }
+    } catch(err) {
+        // console.log(err);
+        res.json(err);
+    }
 });
