e90a808d41c3fb56b1f6d14265601722bffc9a59
Iancu Victor-Iuliu <iancu27victor@gmail.com>
2026-06-08 08:49:45 +0300

    created my first post function that pushes to a database a user account

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,36 @@ dotenv.config();
 import express from "express";
 import createServer from "express";
 import mongoose from "mongoose";
+import User from "./schemas/user.js";
 
 const server = createServer();
 let port = process.env.PORT;
+
+server.use(express.json())
+
+server.listen(port, () => {
+      console.log(`Server up and running on ${port}`);
+    });
+
 mongoose
   .connect(`${process.env.MONGO_URI}`)
   .then(() => {
-    server.listen(port, () => {
-      console.log(`Server up and running on ${port}`);
-    });
     console.log("Burn Fitness Cluj DB Connected successfuly");
   })
   .catch((err) => console.log(err));
 
-server.use(express.json())
 
 server.get("/api/users", (req, res) => {
   res.send(users);
+});
+
+server.post('/api/users/register', async (req, res) => {
+    console.log(req.body);
+    try {
+        await User.create({ username: req.body.username, password: req.body.password, email: req.body.email })
+    } catch(err) {
+        console.log(err)
+        return res.json({ message: 'error creating user' })
+    }
+    res.json({ message: 'user created' })
 });
