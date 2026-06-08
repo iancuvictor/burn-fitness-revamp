91cd1baa7976f9d1533321cd5858a9b3d3887570
Iancu Victor-Iuliu <iancu27victor@gmail.com>
2026-06-08 09:45:52 +0300

    moved routes to separate file

---
+++ backend/routes/users.js
@@ -0,0 +1,48 @@
+import express from "express";
+import User from "../schemas/user.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send(users);
+});
+
+router.post('/register', async (req, res) => {
+    console.log(req.body);
+    let hashedPass = await bcrypt.hash(req.body.password, 10)
+    try {
+        await User.create({ username: req.body.username, password: hashedPass, email: req.body.email });
+        console.log(`User with username: ${req.body.username} has registered now.`)
+    } catch(err) {
+        console.log(err)
+        return res.json({ message: 'error creating user' })
+    }
+    res.json({ message: 'user created' })
+});
+
+router.post('/login', async (req, res) => {
+    // checks if user exists
+    let userFound = await User.findOne({$or: [{username: req.body.username},{ email: req.body.email}]})
+    // try catch so it doesn't crash
+    try {
+        if(userFound !== null){
+            if(await bcrypt.compare(req.body.password, userFound.password)){
+                let signature = jwt.sign({username: userFound.username}, process.env.SIGN_KEY, { expiresIn: '30d' })
+                console.log(signature);
+                res.json(signature);
+            } else {
+                console.log('Wrong password');
+                res.json('Wrong password');
+            }
+        } else if(userFound === null){
+            console.log('no user with this name or email is associated');
+            res.json('User not found');
+        }
+    } catch(err) {
+        // console.log(err);
+        res.json(err);
+    }
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,7 @@ dotenv.config();
 import express from "express";
 import createServer from "express";
 import mongoose from "mongoose";
-import User from "./schemas/user.js";
-import bcrypt from 'bcrypt';
+import userRoutes from './routes/users.js';
 
 const server = createServer();
 let port = process.env.PORT;
@@ -22,43 +21,4 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-
-server.get("/api/users", (req, res) => {
-  res.send(users);
-});
-
-server.post('/api/users/register', async (req, res) => {
-    console.log(req.body);
-    let hashedPass = await bcrypt.hash(req.body.password, 10)
-    try {
-        await User.create({ username: req.body.username, password: hashedPass, email: req.body.email });
-        console.log(`User with username: ${req.body.username} has registered now.`)
-    } catch(err) {
-        console.log(err)
-        return res.json({ message: 'error creating user' })
-    }
-    res.json({ message: 'user created' })
-});
-
-server.post('/api/users/login', async (req, res) => {
-    // checks if user exists
-    let userFound = await User.findOne({$or: [{username: req.body.username},{ email: req.body.email}]})
-    // try catch so it doesn't crash
-    try {
-        if(userFound !== null){
-            if(await bcrypt.compare(req.body.password, userFound.password)){
-                console.log('Access permited');
-                res.json('Access permited');
-            } else {
-                console.log('Wrong password');
-                res.json('Wrong password');
-            }
-        } else if(userFound === null){
-            console.log('no user with this name or email is associated');
-            res.json('User not found');
-        }
-    } catch(err) {
-        // console.log(err);
-        res.json(err);
-    }
-});
+server.use('/api/users', userRoutes);
