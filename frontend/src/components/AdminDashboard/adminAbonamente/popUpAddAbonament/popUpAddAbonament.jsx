9d870b7d47d59d23315760b368a7d2c2a918bce2
Iancu Victor-Iuliu <iancu27victor@gmail.com>
2026-06-14 00:48:43 +0300

    added a semi functioning function that updates the fields (pret and duratie)

--- frontend/src/components/AdminDashboard/adminAbonamente/popUpAddAbonament/popUpAddAbonament.jsx
+++ frontend/src/components/AdminDashboard/adminAbonamente/popUpAddAbonament/popUpAddAbonament.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+const inputStyle = "pt-[5px] pb-[5px] pl-[10px] pr-[10px] border-1";
+
 function PopUpAddAbonament({displayedMenus, setDisplayedMenus}) {
     const defaultFormData = {
         tier: '',
@@ -15,25 +17,51 @@ function PopUpAddAbonament({displayedMenus, setDisplayedMenus}) {
     setDisplayedMenus({...displayedMenus, popUpAddAbonament: false})
   };
 
+  const adaugaPret = () => {
+    const newPreturi = [...formData.preturi, {pret: '', duratie: ''}]
+    setFormData({...formData, preturi: newPreturi})
+  }
+
   const updateForm = (field, value) => {
     setFormData({...formData, [field]: value});
     console.log(formData)
   }
 
+  const updatePret = (field, index, value) => {
+    setFormData({...formData, preturi: formData.preturi.map((pret, i) => {
+        if(i === index){
+            return {...pret, [field]: value};
+        } else {
+            return pret;
+        }
+    })})
+  }
+
   return (
-    <div className="bg-black/80 z-3 fixed top-0 left-0 h-full w-full flex justify-center items-center">
-      <div className="flex flex-col bg-white p-[20px] rounded-md">
-        <div className="flex flex-col">
+    <div className="bg-black/80 z-3 fixed top-0 left-0 h-full w-full flex justify-center items-center font-finlandica">
+      <div className="flex flex-col w-150 h-fit bg-white p-[20px] rounded-md gap-5">
+        <h1 className="font-[600]">Adaugă abonament</h1>
+        <div className="flex flex-col gap-2">
+            <div className="flex gap-2">
+                <span>Tier: </span>
             <select onChange={(e) => updateForm('tier', e.target.value)} name="" id="">
-                <option value="premium">premium</option>
-                <option value="regular">regular</option>
+                <option value="premium">Premium</option>
+                <option value="regular">Regular</option>
             </select>
-            <input onChange={(e) => updateForm('titlu', e.target.value)} type="text" name="" id="" placeholder="titlu"/>
-            <input onChange={(e) => updateForm('desc', e.target.value)} type="text" name="" id="" placeholder="descriere (opțională)"/>
-            <button>Adaugă prețuri</button>
-            <input type="text" name="" id="" placeholder="preț"/>
-            <input type="text" name="" id="" placeholder="durație"/>
-            <input onChange={(e) => updateForm('imagine', e.target.value)} type="text" name="" id="" placeholder="imagine"/>
+            </div>
+            <input className={inputStyle}
+            onChange={(e) => updateForm('titlu', e.target.value)} type="text" name="" id="" placeholder="titlu"/>
+            <input className={inputStyle} onChange={(e) => updateForm('desc', e.target.value)} type="text" name="" id="" placeholder="descriere (opțională)"/>
+            <button className="cursor-pointer" onClick={() => adaugaPret()}>Adaugă prețuri</button>
+            <div className="flex flex-col gap-2">
+                {formData.preturi.map((pret, index) => {
+                    return <div className="flex gap-2" key={index}>
+            <input onChange={(e) => updatePret('pret', index, e.target.value)} className={inputStyle} type="text" name="" id="" value={pret.pret} placeholder="preț"/>
+            <input onChange={(e) => updatePret('duratie', index, e.target.value)} className={inputStyle} type="text" name="" id="" value={pret.duratie} placeholder="durație"/>
+            </div>
+                })}
+            </div>
+            <input className={inputStyle} onChange={(e) => updateForm('imagine', e.target.value)} type="text" name="" id="" placeholder="imagine"/>
         </div>
           <div className="flex justify-between">
             <button
