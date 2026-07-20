459fe68e7464fd9f6471682a252ea01bf62cfb9f
Iancu Victor-Iuliu <iancu27victor@gmail.com>
2026-07-20 18:56:56 +0300

    revert

--- backend/routes/subscriptions.js
+++ backend/routes/subscriptions.js
@@ -5,13 +5,21 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { protect, admin } from "./auth.js";
 import nodemailer from "nodemailer";
-import SibApiV3Sdk from '@sendinblue/client'
 
 const router = express.Router();
 
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-
-
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.BREVO_EMAIL,
+    pass: process.env.BREVO_SMTP_KEY,
+  },
+  family: 4,
+  logger: true,
+  debug: true
+});
 
 router.get("/", async (req, res) => {
   let data = await Abonament.find();
@@ -21,60 +29,41 @@ router.get("/", async (req, res) => {
 router.post("/ziGratis", async (req, res) => {
   let checkAvailability = await User.findOne({ email: req.body.email });
   if (checkAvailability === null) {
-
-    const apiKey = process.env.BREVO_API_KEY;
-      if (!apiKey) {
-        throw new Error("BREVO_API_KEY is not configured");
-      }
-
-      apiInstance.setApiKey(
-        SibApiV3Sdk.ApiClient.instance.authentications['api-key'], 
-        apiKey
-      );
-
-    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail({
-        sender: {
-          name: "Burn Fitness Cluj",
-          email: "burnclujfake@gmail.com"
-        },
-        to: [{
-          email: req.body.email
-        }],
-        subject: "Zi gratuită la Burn Fitness Cluj-Napoca",
-        htmlContent: `
-          <table width="100%" cellpadding="0" cellspacing="0" border="0">
-            <tr>
-              <td align="center">
-                <table width="300" cellpadding="0" cellspacing="0" border="0" style="background: #000; border-radius: 10px; padding: 15px 15px 40px 15px;">
-                  <tr>
-                    <td align="center" style="padding-bottom: 20px;">
-                      <h1 style="font-family: Arial; font-size: 24px; color: white; text-align: center; margin: 0;">Revendică-ți ședința gratuită!</h1>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td style="font-family: Arial; font-size: 18px; color: white; text-align: justify; padding-bottom: 20px;">
-                      Prezintă acest email la recepție! Dacă te decizi să îți continui călătoria în fitness cu noi, poți achiziționa un abonament 
-                      <a style="color: #6776E0;" href="https://www.vercel-app-burnfitnessfake-iancu.com/" target="_blank">aici.</a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td align="center">
-                      <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${req.body.email}" 
-                           width="200" height="200" style="padding-bottom: 20px"/>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td align="center">
-                      <img src="https://i.imgur.com/h3SN8vo.png" width="270" style="border-radius: 6px; display: block;"/>
-                    </td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-          </table>
-        `
-      });
-
+    await transporter.sendMail({
+      from: `"Burn Fitness Cluj" <burnclujfake@gmail.com>`,
+      replyTo: "burnclujfake@gmail.com",
+      to: req.body.email,
+      subject: "Zi gratuită la Burn Fitness Cluj-Napoca",
+      html: `<table width="100%" cellpadding="0" cellspacing="0" border="0">
+  <tr>
+    <td align="center">
+      <table width="300" cellpadding="0" cellspacing="0" border="0" style="background: #000; border-radius: 10px; padding: 15px 15px 40px 15px;">
+        <tr>
+          <td align="center" style="padding-bottom: 20px;">
+            <h1 style="font-family: Arial; font-size: 24px; color: white; text-align: center; margin: 0;">Revendică-ți ședința gratuită!</h1>
+          </td>
+        </tr>
+        <tr>
+          <td style="font-family: Arial; font-size: 18px; color: white; text-align: justify; padding-bottom: 20px;">
+            Prezintă acest email la recepție! Dacă te decizi să îți continui călătoria în fitness cu noi, poți achiziționa un abonament
+            <a style="color: #6776E0;" href="https://www.vercel-app-burnfitnessfake-iancu.com/" target="_blank">aici.</a>
+          </td>
+        </tr>
+        <tr>
+    <td align="center">
+      <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${req.body.email}" width="200" height="200" style='padding-bottom: 20px'/>
+    </td>
+  </tr>
+        <tr>
+          <td align="center">
+            <img src="https://i.imgur.com/h3SN8vo.png" width="270" style="border-radius: 6px; display: block;"/>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>`,
+    });
     res.status(200).json({ message: "success" });
   } else {
     res.status(404).json({ message: "user already exists" });
