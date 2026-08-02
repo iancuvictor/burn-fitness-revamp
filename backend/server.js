d7bcf8de808d6131155fad963d29ec4df3c52799
Iancu Victor-Iuliu <iancu27victor@gmail.com>
2026-08-02 12:12:59 +0300

    added a health check get function in the server

--- backend/server.js
+++ backend/server.js
@@ -22,25 +22,31 @@ server.use(cors({
   credentials: true
 }))
 
+
 server.use(cookieParser())
 server.use('/api/payments', payments);
 server.use(express.json())
 server.use('/api/uploads', express.static('./uploads'));
 
 mongoose
-  .connect(`${process.env.MONGO_URI}`)
-  .then(() => {
-    console.log("Burn Fitness Cluj DB Connected successfuly");
-  })
-  .catch((err) => console.log(err));
+.connect(`${process.env.MONGO_URI}`)
+.then(() => {
+  console.log("Burn Fitness Cluj DB Connected successfuly");
+})
+.catch((err) => console.log(err));
 
 server.use('/api/users', userRoutes);
 server.use('/api/classes', classesRoutes);
 server.use('/api/abonamente', abonamenteRoutes);
 server.use('/api/publicPages', publicRoutes);
 
+server.get('/', (req, res) => {
+  res.status(200).json({message: 'Server is up and running'})
+})
+
 server.listen(port, () => {
       console.log(`Server up and running on ${port}`);
     });
-
+    
     // in case of nodemon problems run: taskkill /f /im node.exe
+    
